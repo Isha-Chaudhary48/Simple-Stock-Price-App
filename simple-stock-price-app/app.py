8f2fda6c03733f89fd643d81838e71cb408cf167
isha <ishachaudhary7876@gmail.com>
2025-10-04 13:24:50 +0000

    added the historical data of Google and added its line charts

--- simple-stock-price-app/app.py
+++ simple-stock-price-app/app.py
@@ -1,4 +1,35 @@
 import streamlit as st
 import pandas as pd
 import yfinance as yf
+st.write(""" 
+# Simple Stock Price App
+         
+Shown are the stock closing price an volume of Google
+         
+         """)
+
+google = yf.Ticker("GOOGL")
+
+google_data = google.history(period = "6mo")
+
+st.write(google_data.head())
+st.write(google_data['Close'].tail())
+
+st.line_chart(google_data.Close)
+st.line_chart(google_data.Volume)
+st.write(
+    """
+# Lets analyse the specific years stock record
+     """
+)
+
+google_particular_data = google.history(start = '2010-05-30' , end = '2025-09-30')
+
+st.line_chart(google_particular_data['Close'])
+
+
+
+
+
+
 
